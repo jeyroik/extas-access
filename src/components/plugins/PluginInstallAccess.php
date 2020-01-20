77b248178a1fae9040203ec2b9ec2bf2ad44c291
jeyroik <jeyroik@gmail.com>
2020-01-20 17:26:37 +0300

    added PluginInstallDefault using

--- src/components/plugins/PluginInstallAccess.php
+++ src/components/plugins/PluginInstallAccess.php
@@ -1,10 +1,10 @@
 <?php
 namespace extas\components\plugins;
 
+use extas\components\access\Access;
 use extas\components\access\AccessOperation;
-use extas\interfaces\packages\IInstaller;
-
-use Symfony\Component\Console\Output\OutputInterface;
+use extas\interfaces\access\IAccessRepository;
+use extas\interfaces\IItem;
 
 /**
  * Class PluginInstallAccess
@@ -12,34 +12,41 @@ use Symfony\Component\Console\Output\OutputInterface;
  * @package extas\components\plugins\access
  * @author jeyroik@gmail.com
  */
-class PluginInstallAccess extends Plugin
+class PluginInstallAccess extends PluginInstallDefault
 {
-    use TInstallMessages;
-
-    const FIELD__ACCESS = 'access';
+    protected $selfUID = '';
+    protected $selfRepositoryClass = IAccessRepository::class;
+    protected $selfSection = 'access';
+    protected $selfName = 'access operation';
+    protected $selfItemClass = Access::class;
 
     /**
-     * @param $installer IInstaller
-     * @param $output OutputInterface
+     * @param $item
+     * @param $packageConfig
      *
-     * @throws
+     * @return string
      */
-    public function __invoke($installer, $output)
+    public function getUidValue(&$item, $packageConfig): string
     {
-        $config = $installer->getPackageConfig();
-        $accessOperations = $config[static::FIELD__ACCESS] ?? [];
+        $operation = new AccessOperation($item);
 
-        foreach ($accessOperations as $accessOperation) {
-            $operation = new AccessOperation($accessOperation);
-            if (!$operation->exists()) {
-                $operation->create();
-                $accessString = $operation->getObject() . '.'
-                    . $operation->getSection() . '.'
-                    . $operation->getSubject() . '.'
-                    . $operation->getOperation();
+        return $operation->getObject() . '.' .
+            $operation->getSection() . '.' .
+            $operation->getSubject() . '.' .
+            $operation->getOperation();
+    }
 
-                $this->installed($accessString, 'access', $output, 'create');
-            }
-        }
+    /**
+     * @param array $item
+     * @param array $serviceConfig
+     * @param \extas\interfaces\repositories\IRepository $repo
+     *
+     * @return IItem|null
+     */
+    protected function findItem($item, $serviceConfig, $repo): ?IItem
+    {
+        $operation = new AccessOperation($item);
+
+        return $operation->exists() ? $operation : null;
     }
 }
